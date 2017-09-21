d1b212523bbb55e3bb839bea027c91e8bafc58a3
David Llanos <davidllanos22@gmail.com>
2017-09-21 10:13:46 +0200

    Fixed QR Code not generating correctly on production

--- authoring-tool/php/qrGenerator.php
+++ authoring-tool/php/qrGenerator.php
@@ -6,7 +6,7 @@ $poiID = $_REQUEST['poiID'];
 $appID = $_REQUEST['appID'];
 $isPre = $_REQUEST['isPre'];
 
-$pre = $isPre ? "pre/" : "";
+$pre = isset($isPre) ? "pre/" : "";
 QRcode::png("https://www.geomotiongames.com/" . $pre . "beaconing/app/app.php?game=" . $appID ."&device=browser&teleport=" . $poiID);
 
 ?>
